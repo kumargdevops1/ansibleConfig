d73fe710b5f98d99d190d78b5e1364ea116b9019
Cloud User <ec2-user@ip-172-31-8-77.ap-south-1.compute.internal>
2020-01-04 16:04:26 +0000

    Added Configuration files

---
+++ tracecollection.sh
@@ -0,0 +1,3 @@
+wget -O dd-java-agent.jar 'https://repository.sonatype.org/service/local/artifact/maven/redirect?r=central-proxy&g=com.datadoghq&a=dd-java-agent&v=LATEST'
+java -jar -javaagent:dd-java-agent.jar
+sudo service datadog-agent restart
